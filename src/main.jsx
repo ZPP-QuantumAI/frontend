0756673396b63e4091f819bdcd256df2fc7317a4
Adam <az430277@students.mimuw.edu.pl>
2024-03-14 19:52:05 +0100

    Fix missing marker image

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import "leaflet/dist/leaflet.css";
+// import "leaflet/dist/leaflet.css"; Try to uncomment in future(and delete stylesheet link in index.html).
 
 import { QueryClient, QueryClientProvider } from "react-query";
 
