a1d05f6abee68554c92789124c94ed440ba86cd8
Adam <az430277@students.mimuw.edu.pl>
2024-04-03 01:12:17 +0200

    Add flying to bounds

--- src/app_ui/results/result-map-leaflet/ResultMapLeaflet.jsx
+++ src/app_ui/results/result-map-leaflet/ResultMapLeaflet.jsx
@@ -54,7 +54,7 @@ function ResultMapContent({ result, bounds }) {
 
   useEffect(() => {
     if (bounds) {
-      map.fitBounds(bounds);
+      map.flyToBounds(bounds)
     }
   }, [bounds, map]);
 
